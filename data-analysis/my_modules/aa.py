788b8765b547094ad43cb117e81d68722c19400e
Gianluca Grasso <93397222+gianlucagrassog@users.noreply.github.com>
2023-05-11 12:10:36 +0200

    dd

---
+++ data-analysis/my_modules/aa.py
@@ -0,0 +1,21 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Data
+data = [56.59, 21.57, 15.25, 7]
+labels = ['Checkout Service', 'Currency Service', 'Recommendation Service', 'Other Services']
+
+# Create pie chart
+plt.figure(figsize=(6,4))
+sns.set_palette("muted")
+
+plt.pie(data, labels=labels, autopct='%1.1f%%')
+plt.show()
+
+
+plt.figure(figsize=(6,4))
+
+sns.barplot(x=data, y=labels, palette="muted", orient='h')
+plt.xlabel('Latency Contribution (%)')
+
+plt.show()
